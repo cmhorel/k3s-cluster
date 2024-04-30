6e888dd559a9df1e66d31a93bb2907a9a2decb91
Charles Horel <charles.horel@gmail.com>
2024-04-30 07:55:17 -0600

    dfsaf

--- temp-local-node/bootstrap.sh
+++ temp-local-node/bootstrap.sh
@@ -1,8 +1,10 @@
 grep 'horel-k0s-1' /etc/hosts || echo '192.168.0.204 horel-k0s-1' >> /etc/hosts
 grep 'horel-k0s-2' /etc/hosts || echo '192.168.0.168 horel-k0s-2' >> /etc/hosts
 grep 'horel-k0s-3' /etc/hosts || echo '192.168.0.4 horel-k0s-3' >> /etc/hosts
+grep 'K3S_TOKEN' /etc/environment || echo "K3S_TOKEN=$K3S_TOKEN" >> /etc/environment
 echo "K3S_TOKEN"
 echo $K3S_TOKEN
+grep 'K3S_URL' /etc/environment || echo "K3S_URL=$K3S_URL" >> /etc/environment
 echo "K3S_URL"
 echo $K3S_URL
 echo "INSTALL_K3S_EXEC"
